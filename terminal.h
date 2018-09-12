355b335b7846c99f696643be080d5dee239451e4
Mikaella <42984505+mikaellafs@users.noreply.github.com>
2018-09-11 23:16:45 -0300

    Update terminal.h

--- terminal.h
+++ terminal.h
@@ -10,43 +10,51 @@ typedef struct celTerm CelTerm;
 typedef struct lsterm LsTerm;
 
 /*Insere um terminal na lista de terminais do NetMap na ultima posicao
-* inputs: ponteiro para o tipo Terminal
+* inputs: nome do terminal e localizacao
 * output: nenhum
 * pre-condicao: terminal existe
 * pos-condicao: terminal inserido no netmap 
 */
-void CadastraTerminal (Terminal* term);
+void CadastraTerminal (char* nomeTerm, char* localizacao);
 
 /*Conecta um terminal a um roteador
-* inputs: ponteiro para o tipo Terminal e para o tipo Roteador
+* inputs: nome do terminal e do roteador
 * output: nenhum
 * pre-condicao: terminal e roteador existem
 * pos-condicao: terminal conectado no roteador
 */
-void ConectaTerminal (Terminal* term, Roteador* rot);
+void ConectaTerminal (char* nomeTerm, char* nomeRot);
 
 /*Desconecta um terminal de um roteador
-* inputs: ponteiro para o tipo Terminal
+* inputs: nome do terminal
 * output: nenhum
 * pre-condicao: terminal existe e esta conectado a algum roteador
 * pos-condicao: terminal desconectado do roteador
 */
-void DesconectaTerminal (Terminal* term);
+void DesconectaTerminal (char* nomeTerm);
 
 /*Remove um terminal da lista de terminais do netmap
-* inputs: ponteiro para o tipo Terminal
+* inputs: nome do terminal
 * output: nenhum
 * pre-condicao: terminal existe
 * pos-condicao: terminal removido do netmap
 */
-void RemoveTerminal (Terminal* term);
+void RemoveTerminal (char* nomeTerm);
 
 /*Verifica se e possivel enviar dados para outro terminal
-* inputs: ponteiro para dois tipos Terminal
+* inputs: nome do terminal origem e destino, respectivamente
 * output: SIM se for possivel e NAO, caso contrario
 * pre-condicao: terminais existem
 * pos-condicao: terminais não sao modificados e foram feitas as verificacoes
 */
-char* EnviarPacoteDados (Terminal* term1, Terminal* term2);
+char* EnviarPacoteDados (char* nomeTerm1, char* nomeTerm2);
+
+/*Conta quantos terminais estão na localizacao de entrada
+* inputs: a string que contenha uma localizacao
+* output: quantidade de terminais naquela regiao
+* pre-condicao: nenhum
+* pos-condicao: nenhum
+*/
+int FrequenciaTerminal (char* localizacao);
 
 #endif /* TERMINAL_H_ */
