44c36ab7b2487a0e4fb2d7859ff9e0e2373c603b
Mikaella <42984505+mikaellafs@users.noreply.github.com>
2018-09-11 23:15:22 -0300

    Update roteador.h

--- roteador.h
+++ roteador.h
@@ -10,35 +10,59 @@ typedef struct celRot CelRot;
 typedef struct lsRot LsRot;
 
 /*Insere um roteador na lista de roteadores do NetMap na ultima posicao
-* inputs: ponteiro para o tipo Roteador
+* inputs: nome do roteador e da operadora
 * output: nenhum
 * pre-condicao: roteador existe
 * pos-condicao: roteador inserido no netmap 
 */
-void CadastraRoteador (Roteador* rot);
+void CadastraRoteador (char* rot, char* operadora, LsRot* listaRot);
 
 /*Remove um roteador na lista de roteadores do NetMap
-* inputs: ponteiro para o tipo Roteador
+* inputs: nome do roteador
 * output: nenhum
 * pre-condicao: roteador existe
 * pos-condicao: roteador removido do netmap 
 */
-void RemoveRoteador (Roteador* rot);
+void RemoveRoteador (char* nomeRot);
 
 /*Conecta um roteador ao outro
-* inputs: ponteiro para 2 tipos roteador
+* inputs: nome dos roteadores
 * output: nenhum
 * pre-condicao: roteadores de entrada existem
 * pos-condicao: roteador1 inserido na lista de roteadores do roteador2 e vice-versa
 */
-void ConectaRoteadores (Roteador* rot1, Roteador* rot2);
+void ConectaRoteadores (char* nomeRot1, char* nomeRot2);
 
 /*Desconecta um roteador do outro
-* inputs: ponteiro para os 2 tipos roteador
+* inputs: nome dos roteadores
 * output: nenhum
 * pre-condicao: roteadores de entrada existem e estao conectados
 * pos-condicao: roteador1 removido da lista de roteadores do roteador2 e vice-versa
 */
-void DesconectaRoteadores (Roteador* rot1, Roteador* rot2);
+void DesconectaRoteadores (char* nomeRot1, char* nomeRot2);
+
+/*Conta quantos roteadores são da operadora de entrada
+* inputs: a string que contenha o nome da operadora
+* output: quantidade de roteadores da operadora
+* pre-condicao: 
+* pos-condicao: 
+*/
+int FrequenciaOperadora (char* operadora);
+
+/*Inicializa um tipo Roteador
+* inputs: a string que contenha o nome do roteador e da operadora
+* output: ponteiro para o roteador inicializado
+* pre-condicao: nenhum
+* pos-condicao: roteador existe e seus componentes foram inicializados
+*/
+Roteador* criaRoteador(char* rot, char* operadora);
+
+/*Inicializa uma lista de Roteadores
+* inputs: nenhum
+* output: ponteiro para a lista
+* pre-condicao: nenhum
+* pos-condicao: lista existe
+*/
+LsRot* InicializaListaRot();
 
 #endif /* ROTEADOR_H_ */
